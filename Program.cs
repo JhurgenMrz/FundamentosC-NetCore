54552936c89d049a476a4da8a42c492c5abcaea5
Jhurgen Maraza <JhurgenMrz99@gmail.com>
2019-03-20 18:20:51 -0300

    Mi primer programa de consola C#

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FundamentosC_NetCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
